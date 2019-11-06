95a4d4041d57a2596a457cdca0562a07b44d9b2e
zhuyuwei <756874340@qq.com>
2019-11-06 15:54:51 +0800

    2.5 前后台登录独立系统

--- frontend/messages/zh-CN/common.php
+++ frontend/messages/zh-CN/common.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'Blog' => '博客',
-    'Signup' => '注册'
+    'Signup' => '注册',
+    'email' => '邮箱'
 ];
--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -3,7 +3,7 @@ namespace frontend\models;
 
 use Yii;
 use yii\base\Model;
-use common\models\User;
+use common\models\UserModel;
 
 /**
  * Signup form
@@ -23,20 +23,25 @@ class SignupForm extends Model
         return [
             ['username', 'trim'],
             ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'unique', 'targetClass' => '\common\models\UserModel', 'message' => '用户名已经存在.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\common\models\UserModel', 'message' => 'This email address has already been taken.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
         ];
     }
-
+    public function attributeLabels() { // 在何处引用了？ 特殊函数?
+        return [
+            'username' => '用户名',
+            'email' => Yii::t('common', 'email')
+        ];
+    }
     /**
      * Signs user up.
      *
@@ -48,7 +53,7 @@ class SignupForm extends Model
             return null;
         }
         
-        $user = new User();
+        $user = new UserModel();
         $user->username = $this->username;
         $user->email = $this->email;
         $user->setPassword($this->password);
